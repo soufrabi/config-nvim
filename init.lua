23f89423bf258603b060e7486bd91bd32532a49e
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-07-29 11:38:32 +0530

    coc-light-config

--- init.lua
+++ init.lua
@@ -23,4 +23,22 @@ vim.cmd('source ~/.config/nvim/vimscript/ale-config.vim')
 vim.cmd('source ~/.config/nvim/vimscript/general.vim')
 vim.cmd('source ~/.config/nvim/vimscript/vim-commentary.vim')
 
+local function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+-- coc color scheme light and dark
+
+if (file_exists('vimscript/coc-config-light.vim'))
+then
+vim.cmd('source ~/.config/nvim/vimscript/coc-config-light.vim')
+end
+
+
+if (file_exists('vimscript/coc-config-dark.vim'))
+then
+vim.cmd('source ~/.config/nvim/vimscript/coc-config-dark.vim')
+end
+
 
