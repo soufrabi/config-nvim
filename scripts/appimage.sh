59e2f70e2ab4e6517fb89aded180b1eb876a0406
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-07-29 11:38:33 +0530

    pass arguments in appimage.sh

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+check_if_user_is_root() {
+
 if [ ! "$USER" = "root" ]; then
 	echo "Please run the command as root user"
 	echo "Script needs elevated priviledges"
@@ -9,9 +11,16 @@ else
 	echo "Running commands as root user"
 fi
 
+}
+
+remove_package() {
 
 rm -rv /opt/neovim-linux64
 unlink /usr/local/bin/nvim
+
+}
+
+install_package() {
 
 mkdir -pv /opt/neovim-linux64
 cd /opt/neovim-linux64
@@ -22,5 +31,31 @@ chmod --verbose +x nvim.appimage
 
 ln -sv /opt/neovim-linux64/squashfs-root/usr/bin/nvim /usr/local/bin
 
+}
 
+
+main() {
+
+if [ ! "$#"  = 1 ] ; then
+	echo "Please provide the operation"
+	exit 1
+fi
+
+
+if [ "$1" = "install" ] ; then
+	echo "Install"
+	check_if_user_is_root
+	install_package
+elif [ "$1" = "clean" ] ; then
+	echo "Clean"
+	check_if_user_is_root
+	remove_package
+else
+	echo "Unknown option"
+fi
+
+
+}
+
+main "$@"
 
