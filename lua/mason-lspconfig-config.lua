30cb52e80c1679f32ea636bcc5626b0ee6ffc646
Anirban Dey <code@soufrabi.com>
2023-12-29 15:57:46 +0530

    do not run ensure_installed in meson

--- lua/mason-lspconfig-config.lua
+++ lua/mason-lspconfig-config.lua
@@ -3,14 +3,14 @@ local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup({
-    ensure_installed= {
-        "tsserver",
-        "html",
-        "cssls",
-        "clangd",
-    },
-    -- auto installed configured server
-    automatic_installation = true, -- not the same as ensure_installed
+    -- ensure_installed= {
+    --     "tsserver",
+    --     "html",
+    --     "cssls",
+    --     "clangd",
+    -- },
+    -- -- auto installed configured server
+    -- automatic_installation = true, -- not the same as ensure_installed
 
 
 })
