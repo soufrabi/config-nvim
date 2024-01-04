9609ef7c4cab8e35e4026080aed10941cc36e6d2
Anirban Dey <code@soufrabi.com>
2024-01-04 17:28:49 +0530

    use /usr/bin/python as path to python

--- lua/python-general.lua
+++ lua/python-general.lua
@@ -1,11 +1,14 @@
 -- General python
 
-
--- vim.g.python_host_prog="/home/darklord/.venvs/.nvim-venv/bin/python"
--- vim.g.python3_host_prog="/home/darklord/.venvs/.nvim-venv/bin/python"
-
 if G.getOS() == "Linux" then
-   vim.g.python_host_prog = G.home_dir .. "/.venvs/.nvim-venv/bin/python"
-   vim.g.python3_host_prog = G.home_dir .. "/.venvs/.nvim-venv/bin/python"
-end
+    -- if os.getenv("DISTROBOX_HOST_HOME") == nil then
+    --     vim.g.python_host_prog = G.home_dir .. "/.venvs/.nvim-venv/bin/python"
+    --     vim.g.python3_host_prog = G.home_dir .. "/.venvs/.nvim-venv/bin/python"
+    -- elseif G.dir_exists(os.getenv("DISTROBOX_HOST_HOME")) then
+    --     vim.g.python_host_prog = os.getenv("DISTROBOX_HOST_HOME").. "/.venvs/.nvim-venv/bin/python"
+    --     vim.g.python3_host_prog = os.getenv("DISTROBOX_HOST_HOME") .. "/.venvs/.nvim-venv/bin/python"
+    -- end
 
+    vim.g.python_host_prog = "/usr/bin/python"
+    vim.g.python3_host_prog = "/usr/bin/python"
+end
