7b0193efdeac4e1a9cc08c08d58984f21f104512
Anirban Dey <code@soufrabi.com>
2024-01-06 17:47:43 +0530

    vim-commentary for dosinit distrobox files
    use # for comment not ;

--- lua/vim-commentary-config.lua
+++ lua/vim-commentary-config.lua
@@ -1,9 +1,9 @@
+--  Vim Commentary
 
 -- autocmd BufNewFile,BufRead *.md,*.mkdn,*.markdown :set filetype=markdown
 -- autocmd BufNewFile,BufRead *.cpp :set filetype=cpp
--- 
 -- autocmd FileType apache setlocal commentstring=#\ %s
-
 -- autocmd FileType cpp setlocal commentstring=//\ %s
 
 vim.cmd('autocmd FileType cpp setlocal commentstring=//\\ %s')
+vim.cmd('autocmd FileType dosini setlocal commentstring=#\\ %s')
