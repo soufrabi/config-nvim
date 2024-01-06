70899093c9869b114fcad1c74c09c51a1423d801
Anirban Dey <code@soufrabi.com>
2024-01-06 17:55:24 +0530

    remove reference to coc-config-light/dark
    as coc is not being used for lsp completion

--- lua/vimscripts.lua
+++ lua/vimscripts.lua
@@ -1,21 +1,4 @@
-
-
-
--- Coc color schemes : light or dark
-
-if (G.path_exists(G.config_dir..'/vimscript/coc-config-light.vim'))
-then
-    vim.cmd('source '..G.config_dir..'/vimscript/coc-config-light.vim')
-end
-
-
-if (G.path_exists(G.config_dir .. '/vimscript/coc-config-dark.vim'))
-then
-    vim.cmd('source '..G.config_dir..'/vimscript/coc-config-dark.vim')
-end
-
-
--- Testing
+-- Testing vimscript
 
 if (G.path_exists(G.config_dir .. '/vimscript/test.vim'))
 then
